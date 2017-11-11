796ea5d171ec5563c7de464832b8b5e87fdee372
Jorge Barrios <jorgebarmza@gmail.com>
2017-11-10 20:32:49 -0600

    Implement figure drawing algorithms based on ddl

--- shape-transformer/sketch.js
+++ shape-transformer/sketch.js
@@ -11,21 +11,7 @@ function setup() {
   fill(256,256,256);
   noFill();
   stroke(256,0,0);
-
-  // ddaLine(300,300,50,50);
-  // line(90,90,340,340);
 }
-
-// function setup3d() {
-//   var canvas =   createCanvas(500, 500, WEBGL);
-//   // Move the canvas so it's inside our <div id="sketch-holder">.
-//   canvas.parent('sketch-holder');
-//
-//   background(0,0,128);
-//   fill(256,256,256);
-//   noFill();
-//   stroke(256,0,0);
-// }
 
 function draw() {
 
@@ -33,11 +19,6 @@ function draw() {
 
 function clearUI(){
   location.reload();
-  // setup();
-  // let div = document.getElementById('shape-container');
-  // div.style.display = 'flex';
-  // div = document.getElementById('transformations-container');
-  // div.style.display = 'none';
 }
 
 function mouseReleased(){
@@ -57,7 +38,7 @@ function display(id){
   divToHide.style.display = 'flex';
 }
 
-function ddaLine(x1, y1, x2, y2){
+function ddlLine(x1, y1, x2, y2){
   let dx = x2-x1;
   let dy = y2-y1;
 
@@ -74,6 +55,59 @@ function ddaLine(x1, y1, x2, y2){
     y+=yInc;
     point(x,y);
   }
+}
+
+function myRect(x1,y1,w,h){
+  ddlLine(x1,y1,x1+w,y1);
+  ddlLine(x1,y1,x1,y1+w);
+  ddlLine(x1+w,y1+h,x1+w,y1);
+  ddlLine(x1+w,y1+h,x1,y1+h);
+}
+
+function myTriangle(x1,y1,x2,y2,x3,y3){
+  ddlLine(x1,y1,x2,y2);
+  ddlLine(x1,y1,x3,y3);
+  ddlLine(x2,y2,x3,y3);
+}
+
+function midPointCircle(centerX, centerY, r){
+    let x = r;
+    let y = 0;
+
+    point(x + centerX, y + centerY);
+
+    if (r > 0){
+        point(x + centerX, -y + centerY);
+        point(y + centerY, x + centerX);
+        point(-y + centerY, x + centerX);
+    }
+
+    let P = 1 - r;
+    while (x > y){
+        y++;
+        if (P <= 0){
+            P = P + 2*y + 1;
+        }
+        else{
+            x--;
+            P = P + 2*y - 2*x + 1;
+        }
+
+        if (x < y)
+            break;
+
+        point(x + centerX, y + centerY);
+        point(-x + centerX, y + centerY);
+        point(x + centerX, -y + centerY);
+        point( -x + centerX, -y + centerY);
+
+        if (x != y){
+            point(y + centerY, x + centerX);
+            point(-y + centerY, x + centerX);
+            point(y + centerY, -x + centerX);
+            point( -y + centerY, -x + centerX);
+        }
+    }
 }
 
 function midPointArc(centerX, centerY, r){
@@ -127,21 +161,13 @@ function drawPolygon(){
   drawingPoligon = true;
 }
 
-function drawCube(){
-  hide('shape-container');
-  display('transformations-container');
-  setup3d();
-  box(100, 100, 100);
-  lastDrawF = drawCube;
-}
-
 function drawArc(){
   hide('shape-container');
   display('transformations-container');
-  // arc(255, 255, 200, 200, PI, 0);
   midPointArc(255, 255, 200);
   lastDrawF = drawArc;
 }
+
 function drawCube(){
   hide('shape-container');
   display('transformations-container');
@@ -149,12 +175,12 @@ function drawCube(){
   let y1 = 100;
   let s = 100;
   let c = s/2;
-  rect(x1,y1,s,s);
-  rect(x1+c,y1+c,s,s);
-  line(x1,y1,x1+c,y1+c);
-  line(x1+s,y1,x1+c+s,y1+c);
-  line(x1,y1+s,x1+c,y1+s+c);
-  line(x1+s,y1+s,x1+s+c,y1+s+c);
+  myRect(x1,y1,s,s);
+  myRect(x1+c,y1+c,s,s);
+  ddlLine(x1,y1,x1+c,y1+c);
+  ddlLine(x1+s,y1,x1+c+s,y1+c);
+  ddlLine(x1,y1+s,x1+c,y1+s+c);
+  ddlLine(x1+s,y1+s,x1+s+c,y1+s+c);
   lastDrawF = drawCube;
 }
 function drawRectPrism(){
@@ -167,15 +193,14 @@ function drawRectPrism(){
   let c = 100;
   rect(x1,y1,w,h);
   rect(x1+c,y1+c,w,h);
-  line(x1,y1,x1+c,y1+c);
-  line(x1+w,y1,x1+w+c,y1+c);
-  line(x1,y1+h,x1+c,y1+h+c);
-  line(x1+w,y1+h,x1+w+c,y1+h+c);
+  ddlLine(x1,y1,x1+c,y1+c);
+  ddlLine(x1+w,y1,x1+w+c,y1+c);
+  ddlLine(x1,y1+h,x1+c,y1+h+c);
+  ddlLine(x1+w,y1+h,x1+w+c,y1+h+c);
   lastDrawF = drawRectPrism;
 }
 
 function drawTriangPrism(){
-
   hide('shape-container');
   display('transformations-container');
   let x1 = 250;
@@ -185,11 +210,11 @@ function drawTriangPrism(){
   let x3 = 300;
   let y3 = 300;
   let c = 100;
-  triangle(x1,y1,x2,y2,x3,y3);
-  triangle(x1+c,y1+c,x2+c,y2+c,x3+c,y3+c);
-  line(x1,y1,x1+c,y1+c);
-  line(x2,y2,x2+c,y2+c);
-  line(x3,y3,x3+c,y3+c);
+  myTriangle(x1,y1,x2,y2,x3,y3);
+  myTriangle(x1+c,y1+c,x2+c,y2+c,x3+c,y3+c);
+  ddlLine(x1,y1,x1+c,y1+c);
+  ddlLine(x2,y2,x2+c,y2+c);
+  ddlLine(x3,y3,x3+c,y3+c);
   lastDrawF = drawTriangPrism;
 }
 
@@ -198,11 +223,11 @@ function drawCone(){
   display('transformations-container');
   let x1 = 250;
   let y1 = 250;
-  let w = 100;
+  let w = 25;
   let h = w/4
-  ellipse(x1,y1,w,h);
-  line(x1-w/2,y1,x1,y1-50);
-  line(x1+w/2,y1,x1,y1-50)
+  midPointCircle(x1,y1,w);
+  ddlLine(x1-w,y1,x1,y1-50);
+  ddlLine(x1+w,y1,x1,y1-50)
   lastDrawF = drawCone;
 }
 
@@ -210,13 +235,13 @@ function finishPolygon(){
   for(i = 0; i<vertices.length-2; ++i){ // avoid last vertex (button click)
     var p1 = vertices[i];
     var p2 = vertices[i+1];
-    line(p1[0],p1[1],p2[0],p2[1]);
+    ddlLine(p1[0],p1[1],p2[0],p2[1]);
   }
 
   //join first and last vertices
   var p1 = vertices[vertices.length-2];
   var p2 = vertices[0];
-  line(p1[0],p1[1],p2[0],p2[1]);
+  ddlLine(p1[0],p1[1],p2[0],p2[1]);
 
   // info back to normal
   drawingPoligon = false;
@@ -275,7 +300,7 @@ function scaleM(){
 
 function reflectM(){
   stroke(0,255,0);
-  line(0,0,500,500);
+  ddlLine(0,0,500,500);
   stroke(256,0,0);
   applyMatrix(0, 1, 1, 0, 0, 0);
   lastDrawF();
